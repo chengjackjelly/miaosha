09b00b24288f6caf8ef494af7548561f208ede19
“jack” <601427268@qq.com>
2023-03-15 13:31:06 +0800

    token-based varitification

--- src/main/java/com/miaosha/controller/OrderController.java
+++ src/main/java/com/miaosha/controller/OrderController.java
@@ -8,8 +8,11 @@ import com.miaosha.response.CommonReturnType;
 import com.miaosha.service.OrderService;
 import com.miaosha.service.model.OrderModel;
 import com.miaosha.service.model.UserModel;
+import org.apache.catalina.User;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,16 +31,21 @@ public class OrderController {
     @Autowired
     private HttpServletRequest httpServletRequest;
 
+    @Autowired
+    private RedisTemplate redisTemplate;
+
     @RequestMapping("/createorder")
     @ResponseBody
-    public CommonReturnType createOrder(@RequestParam(name="itemId") Integer itemId,@RequestParam(name="promoId") Integer promoId,@RequestParam(name="amount") Integer amount) throws BusinessException {
-        Boolean isLogin=(Boolean)httpServletRequest.getSession().getAttribute("IS_LOGIN");
-        if(isLogin==null){
-            System.out.println("here");
-            throw new BusinessException(EmBussineseError.USER_NOT_EXIST);
-//            throw new BusinessException(EmBussineseError.USER_NOT_LOGIN,"test");
+    public CommonReturnType createOrder(@RequestParam(name="itemId") Integer itemId,@RequestParam(name="amount") Integer amount,@RequestParam(name="promoId",required = false) Integer promoId) throws BusinessException {
+        String token = httpServletRequest.getParameterMap().get("token")[0];
+        if(StringUtils.isEmpty(token)){
+            throw new BusinessException(EmBussineseError.USER_NOT_LOGIN,"未登录");
         }
-        UserModel userModel= (UserModel)httpServletRequest.getSession().getAttribute("LOGIN_USER");
+        UserModel userModel=(UserModel) redisTemplate.opsForValue().get(token);
+        if(userModel == null ){
+            throw  new BusinessException(EmBussineseError.USER_NOT_LOGIN,"未登录");
+        }
+
         OrderModel orderModel=orderService.createOrder(itemId,userModel.getId(),amount,promoId);
 
         return CommonReturnType.create(null);
